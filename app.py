9f3d55a51cd4f761adff65c5b1b6f277facedcb9
Irtaza Ahmed <irtazaahmad2715@gmail.com>
2026-08-03 18:16:25 +0500

    Fix import statement for gTTS module

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import gradio as gr
 from groq import Groq
-from gttS import gTTS
+from gtts import gTTS
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
